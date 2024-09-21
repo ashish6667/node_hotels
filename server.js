1d9798c9eb2a68f79a4149a28336863a02067b35
ashish6667 <ay1060045@.com>
2024-09-21 14:10:01 +0530

    new line add

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express();
 const db = require('./db');
+require('dotenv').config();
+
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // req.body
+const PORT = process.env.PORT || 3000;
 
 
 app.get('/', function (req, res){
@@ -23,8 +26,7 @@ app.use('/person', personRoutes);
 app.use('/menu',menuItemRoutes);
 
 
-
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log('listening on port 3000');
     
 })
